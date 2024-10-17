c8b67ca916086ebbf133280db9291fda80c9cfeb
Dave Jung <davejinyoung@gmail.com>
2024-10-17 13:31:03 -0700

    More db configs and created bare minimum patient model

--- mister_ed/mister_ed/mister_ed/models.py
+++ mister_ed/mister_ed/mister_ed/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    date_of_birth = models.DateField(null=False, blank=False)
+    phone_number = models.CharField(null=False, max_length=10)
