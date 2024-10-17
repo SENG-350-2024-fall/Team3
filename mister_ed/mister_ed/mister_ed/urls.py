d0670d78c9e778cfe8eba67094e439fef4758182
Dave Jung <davejinyoung@gmail.com>
2024-10-17 11:30:56 -0700

    temporary pages added

--- mister_ed/mister_ed/mister_ed/urls.py
+++ mister_ed/mister_ed/mister_ed/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("home/", views.home, name="home"),
+    path("triage/", views.triage, name="triage"),
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
 ]
--- mister_ed/mister_ed/mister_ed/views.py
+++ mister_ed/mister_ed/mister_ed/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("TESTING HOME PAGE")
+    return HttpResponse("TEMP HOME PAGE")
+
+def triage(request):
+    return HttpResponse("TEMP TRIAGE PAGE")
+
+def login(request):
+    return HttpResponse("TEMP LOGIN PAGE")
+
+def signup(request):
+    return HttpResponse("TEMP SIGNUP PAGE")
