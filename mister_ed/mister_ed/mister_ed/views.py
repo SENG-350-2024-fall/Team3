1996b23f7e51c435fd3bf181ae99118fe5d64189
Daniel Alvarez <dalvarezrivera@uvic.ca>
2024-10-17 22:48:32 -0700

    Log In first pass

--- mister_ed/mister_ed/mister_ed/views.py
+++ mister_ed/mister_ed/mister_ed/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.views import LoginView
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    
 def home(request):
     return HttpResponse("TEMP HOME PAGE")
 
