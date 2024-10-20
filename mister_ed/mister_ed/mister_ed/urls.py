901220ef8b7ed7d94e7a854b2e45ffd34369fff3
Dave Jung <davejinyoung@gmail.com>
2024-10-19 22:10:38 -0700

    Signup and Login Pages Configuration (#2)

--- mister_ed/mister_ed/mister_ed/urls.py
+++ mister_ed/mister_ed/mister_ed/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
     path("home/", views.home, name="home"),
     path("triage/", views.triage, name="triage"),
     path("login/", views.login, name="login"),
--- mister_ed/mister_ed/mister_ed/views.py
+++ mister_ed/mister_ed/mister_ed/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from .forms import RegisterForm
 
+@login_required(login_url="/login") 
 def home(request):
-    return HttpResponse("TEMP HOME PAGE")
+    return render(request, 'home/home.html')
 
 def triage(request):
     return HttpResponse("TEMP TRIAGE PAGE")
 
 def login(request):
-    return HttpResponse("TEMP LOGIN PAGE")
+    return render(request, 'registration/login.html')
 
 def signup(request):
-    return HttpResponse("TEMP SIGNUP PAGE")
+    if request.user.is_authenticated:
+        return redirect('/home')
+    elif request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('/home')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/signup.html', {"form": form})
